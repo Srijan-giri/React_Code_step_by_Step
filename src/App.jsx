e1e372701b3204e84d661cc65281c7e4ebda029d
srijan_123 <srijan.giri@vyrazu.com>
2025-04-24 12:14:54 +0530

    toggle and show feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import {useState} from 'react';
+import UserComponent from './component/UserComponent';
+
 function App(){
+  const [display,setDisplay] = useState(true);
   return(
     <div>
       <h1>Hello React</h1>
+      <h2>Toggle and Hide Show</h2>
+      <button onClick={()=>setDisplay(!display)}>Toggle</button>
+      {
+        display?<UserComponent/>:null
+      }
+
+     
     </div>
   )
 }
---
+++ src/component/UserComponent.jsx
@@ -0,0 +1,9 @@
+function UserComponent(){
+    return(
+        <div>
+            <h1>User Component</h1>
+        </div>
+    )
+}
+
+export default UserComponent;
