5de9b6c426796119cb5ea188d4fa328456913e73
srijan123 <srijangiri2003@gmail.com>
2025-04-25 07:38:37 +0530

    pass jsx to props

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,29 @@
-import College from "./components/College";
-import Student from "./components/Student";
-import { useState } from "react";
-import Teacher from "./components/Teacher";
+import User from "./components/User";
+import Wrapper from "./components/Wrapper";
 
-function App(){
-  let name = "Srijan Giri";
-  let age = 20;
-  let email = "srijangiri2003@gmail.com";
-
-  let student1 = {
-    name : "Srijan Giri",
-    age : 20,
-    email : "srijangiri2003@gmail.com"
-  }
-
-  let student2 = {
-    name : "Anand Singh",
-    age : 30,
-    email : "anand@gmail.com"
-  }
-
-  let student3 = {
-    name : "Axar Patel",
-    age : 40,
-    email : "axar3@gmail.com"
-  }
-
-
-  let college = ['MIT','IIT','NIT','DU','JU'];
-
-  const[teacher,setTeacher] = useState();
-  return(
+function App() {
+  return (
     <div>
-      <h1>Props in React</h1>
+      <h1>Props pass to JSX</h1>
+      <User name={"Anil Sadhu"} />
+      <User />
+      <User name="Srijan Giri" />
 
-      {/*
-           Send Variable
-       */}
-      {/* <Student name={"Srijan Giri"} age={20} email={"srijangiri2003@gmail.com"}/> */}
-      {/* <Student  name={name} age={age} email={email}/> */}
+      <Wrapper color={"yellow"} name="Happy">
+        <h1>Hello Anil</h1>
+      </Wrapper>
 
+      <Wrapper color={"blue"}>
+        <h1>Hello Subham</h1>
+      </Wrapper>
 
-      {/*
-           Send Object
-       */}
-
-      <Student student={student1}/>
-      <Student student={student2}/>
-      <Student student={student3}/>
-      
-      {/*
-           Send Array
-       */}
-
-       <College college = {college}/>
-
-
-       {/*
-           Send Data in Commponent With Click
-       */}
-
-       <button onClick={()=>setTeacher('Ajay Kumar')}>Update Teacher</button>
-       {teacher && <Teacher name={teacher}/>}
-
-
-
+      <Wrapper>
+        <h1>Hello Admin</h1>
+        <h2 style={{ color: 'red', }}>Please Login</h2>
+      </Wrapper>
     </div>
   )
+
 }
 
 export default App;
