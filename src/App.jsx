79c0e0e52ccbb2233272ce78703dd0318a55ac5e
srijan_123 <srijan.giri@vyrazu.com>
2025-04-28 15:32:53 +0530

    useEffect in Life Cycle Method

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+
 function App(){
+  const [counter,setCounter] = useState(0);
+  const [data,setData] = useState(0);
+  const [isDisplay,setIsDisplay] = useState(true);
   return(
     <div>
-      <h1>Hello React</h1>
+      <h1>useEffect in Life Cycle Method</h1>
+      
+      {
+        isDisplay && <Counter counter={counter} data={data}/>
+      }
+      
+      <button onClick={()=>setCounter(counter+1)}>Counter {counter}</button>
+      <button onClick={()=>setData(data+1)}>Data {data}</button>
+      <button onClick={()=>setIsDisplay(!isDisplay)}>Toggle</button>
     </div>
   )
 }
 
-export default App;
+export default App; 
---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+function Counter({counter,data}){
+
+    // mounting
+    useEffect(()=>{
+        console.log("Mounting Phase Only........");
+    },[]);
+
+    // updating
+    useEffect(()=>{
+        console.log('Updating Phase Only.........');
+    },[counter]);
+
+
+    // Deleting
+    useEffect(()=>{
+        return ()=>{
+            console.log('Deleting or Unmounting Phase Only..........');
+        }
+    },[]);
+
+    return(
+        <div>
+            <h1>Counter : {counter}</h1>
+            <h1>Data : {data}</h1>
+        </div>
+    );
+}
+
+export default Counter;
