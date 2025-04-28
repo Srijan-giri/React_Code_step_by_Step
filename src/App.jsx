ad63ea087b21a6c766362796390e4419468fff99
srijan_123 <srijan.giri@vyrazu.com>
2025-04-28 12:54:48 +0530

    useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
+
 function App(){
+  const [counter,setCounter] = useState(0);
+  const [data,setData] = useState(0);
+
+  // call every time
+
+  useEffect(()=>{
+    callEveryTime();
+  })
+
+  // call only once
+  useEffect(()=>{
+    callOnlyOnce();
+  },[]);
+
+
+  //call only when the state is updated
+
+  useEffect(()=>{
+    counterFunction()
+  },[counter]);
+
+
+  // call only counter and data state is updated
+  useEffect(()=>{
+    dataFunction();
+  },[counter,data]);
+
+  function callEveryTime(){
+    console.log("call every time of callEveryTime method")
+  }
+
+  function callOnlyOnce(){
+    console.log('calling call only once method');
+  }
+
+  function counterFunction(){
+    console.log("Counter Function : ",counter);
+  }
+
+  function dataFunction(){
+    console.log('counter and data state updated');
+    console.log("Data Function : ",data);
+  }
+
+  
   return(
     <div>
-      <h1>Hello React</h1>
+      <h1>useEffect Hook</h1>
+      <button onClick={()=>setCounter(counter+1)}>Counter {counter}</button>
+      <button onClick={()=>setData(data+1)}>Data {data}</button>
     </div>
   )
 }
