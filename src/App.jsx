57317a7445f9cebdd9ec3177fcd673e32af7804c
srijan_123 <srijan.giri@vyrazu.com>
2025-04-28 16:29:21 +0530

    Inline CSS in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,127 @@
-function App(){
-  return(
+function App() {
+  const myStyle={
+    border: '1px solid #ccc',
+    width: '200px',
+    margin:' 15px',
+    boxShadow: '1px 2px 3px 0px #cccccc99'
+  }
+  return (
     <div>
-      <h1>Hello React</h1>
+      <h1 style={{ color: "red" }}>Inline CSS in React</h1>
+      <div style={{display: 'flex',flexWrap: 'wrap'}}>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+        <div style={myStyle}>
+            <img
+              style={{ width: "200px" }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="image"
+            />
+            <div style={{padding:'5px'}}>
+              <h4>Anil Sidhu</h4>
+              <h4>Software Devoloper</h4>
+            </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
