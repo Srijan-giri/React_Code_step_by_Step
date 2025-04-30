07c76d0542830bcafd587ff8bf9ce1fa4ba42a5f
srijan_123 <srijan.giri@vyrazu.com>
2025-04-30 12:26:57 +0530

    External CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,123 @@
-function App(){
-  return(
+import "./css/style.css";
+
+function App() {
+  return (
     <div>
-      <h1>Hello React</h1>
+      <h1 className="heading">External CSS</h1>
+      <div className="container">
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="div-design">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="image"
+            className="image"
+          />
+          <div className="text-wrap">
+            <h3>Anil Sadhu</h3>
+            <p>Software Developer</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
