4052ece0d6ae0f387ca41e1a86f731b2d9ef0ac0
srijan_123 <srijan.giri@vyrazu.com>
2025-04-30 15:23:56 +0530

    styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import styled from 'styled-components';
+
 function App(){
+
+  // const Heading = styled.h1`
+  //   color:red;
+  //   border:2px solid green;
+  //   border-radius:10px;
+  //   padding:20px;
+  //   margin:10px;
+  // `;
+
+  const Heading = styled.h1({
+        color:'red',
+        border:'2px solid green',
+        borderRadius:'10px',
+        padding:'20px',
+        margin:'10px'
+  });
+
+  const Button = styled.button({
+    color: 'red',
+    width: '100px',
+    height: '38px',
+    padding: '5px',
+    margin: '10px'
+  });
+
+  const P = styled.p({
+    color:'red',
+    fontWeight:'bold'
+  })
   return(
     <div>
-      <h1>Hello React</h1>
+      <h1>Styled Component With React JS</h1>
+      <Heading>Hello Heading</Heading>
+      <Heading>Hello Heading1</Heading>
+      <Heading>Hello Heading2</Heading>
+      <Heading>Hello Heading3</Heading>
+      <Heading>Hello Heading4</Heading>
+      <Heading>Hello Heading5</Heading>
+      <Button>Login</Button>
+      <P>Hello Mic Testing</P>
     </div>
   )
 }
