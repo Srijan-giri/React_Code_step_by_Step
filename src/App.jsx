329b371a6c177a8caba5ae3da634fd489fb93c00
srijan123 <srijangiri2003@gmail.com>
2025-05-06 00:59:12 +0530

    use Fragment in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-function App(){
-  return(
-    <div>
-      <h1>Hello React</h1>
-    </div>
+import { Fragment } from "react";
+
+function App() {
+  return (
+    // ! to remove extra div element -> we have to use Fragment
+    <Fragment>
+      <Data />
+      <Data />
+      <Data />
+
+      <hr />
+
+
+      <Show />
+      <Show />
+    </Fragment>
+  )
+}
+
+function Data() {
+  return (
+    <>
+      <h1>Fragment in React JS</h1>
+      <h1>Fragment in React JS</h1>
+    </>
+  )
+}
+
+
+function Show() {
+  return (
+
+    //! remove div and use Fragment
+    <>
+      <span>Hello <b>User</b></span>
+    </>
   )
 }
 
