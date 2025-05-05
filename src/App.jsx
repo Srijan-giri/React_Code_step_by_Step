78fe7bd7d96453c8a151fb88fef76acd2d2d87dd
srijan_123 <srijan.giri@vyrazu.com>
2025-05-05 18:55:33 +0530

    useActionState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-function App(){
-  return(
+import { useActionState } from "react";
+
+function App() {
+
+  const handleSubmit = async (previousData, formData) => {
+    let name = formData.get('name');
+    let password = formData.get('password');
+    await new Promise((res) => setTimeout(res, 2000));
+    if (name && password) {
+      return { message: 'Data Submitted', name, password }
+    }
+    else {
+      return { error: 'Failed to submit.Enter proper data' }
+    }
+  }
+
+  const [data, action, pending] = useActionState(handleSubmit, undefined);
+  console.log(data);
+  return (
     <div>
-      <h1>Hello React</h1>
+      <h1>useActionState Hook in React</h1>
+      <form action={action}>
+        <input type="text" name="name" placeholder="Enter your name" defaultValue={data?.name} />
+        <br /><br />
+        <input type="password" name="password" placeholder="Enter your password" defaultValue={data?.password} />
+        <br /><br />
+        <button disabled={pending}>
+          {
+            pending ? 'Submitting...' : 'Submit'
+          }
+        </button>
+        <br />
+        {
+          data?.error && <span style={{ color: 'red' }}>{data?.error}</span>
+        }
+        {
+          data?.message && <span style={{ color: 'green' }}>{data?.message}</span>
+        }
+      </form>
+
+      <h3>Name : {data?.name}</h3>
+      <h3>Password : {data?.password}</h3>
+
     </div>
   )
 }
