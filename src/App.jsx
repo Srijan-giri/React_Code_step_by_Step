540a7a71e76742097f5a5124c439aaf8d062882e
srijan123 <srijangiri2003@gmail.com>
2025-05-06 00:28:07 +0530

    useId hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-function App(){
-  return(
+import { useId } from "react";
+
+function App() {
+
+  return (
     <div>
-      <h1>Hello React</h1>
+      <h1>useId hook in React</h1>
+      <UserForm />
+      <hr />
+      <UserForm />
+    </div>
+  )
+}
+
+function UserForm() {
+
+  //* useId is a React Hook for generating unique IDs that can be passed to accessibility attributes.
+
+  // const name = useId();
+  // const password = useId();
+  // const skills = useId();
+  // const terms = useId();
+
+  const user = useId();
+
+  return (
+    <div>
+      <form action="">
+        <label htmlFor={user + "name"}>Enter Name : </label>
+        <input type="text" placeholder="Enter Name" id={user + "name"} />
+        <br /><br />
+        <label htmlFor={user + "password"}>Enter Password : </label>
+        <input type="password" placeholder="Enter Password" id={user + "password"} />
+        <br /><br />
+        <label htmlFor={user + "skills"}>Enter Skills : </label>
+        <input type="text" placeholder="Enter Name" id={user + "skills"} />
+        <br /><br />
+        <input type="checkbox" name="terms" id={user + "terms"} />
+        <label htmlFor={user + "terms"}>Terms and Condition</label>
+
+      </form>
     </div>
   )
 }
