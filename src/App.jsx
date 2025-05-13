ecfc2df39f373626a8eddc34a024b3a7cd4b291a
srijan123 <srijangiri2003@gmail.com>
2025-05-13 22:06:54 +0530

    react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App(){
-  return(
-    <div>
-      <h1>Hello React</h1>
-    </div>
-  )
+import { Link, Route, Routes } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+    return (
+        <div>
+            <h1>React Router 7 with React 19</h1>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <br />
+            <Routes>
+                <Route path="/home" element={<Home />}></Route>
+                <Route path='/about' element={<About />}> </Route>
+            </Routes>
+        </div>
+    )
 }
 
 export default App;
