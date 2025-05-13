dba6e14777650ba46d742a1f36d8cd26c550267b
srijan123 <srijangiri2003@gmail.com>
2025-05-14 00:39:57 +0530

    Navbar and Heading Website using React Router

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+function Navbar() {
+    return (
+        <div className='header'>
+            <div>
+                <Link to={"/"} className='link'> <h2>Logo</h2></Link>
+            </div>
+            <div>
+                <ul>
+                    <li><Link to={'/'} className='link'>Home</Link></li>
+                    <li><Link to={'/about'} className='link'>About</Link></li>
+                    <li><Link to={'/contact'} className='link'>Contact</Link></li>
+                    <li><Link to={'/login'} className='link'>Login</Link></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
