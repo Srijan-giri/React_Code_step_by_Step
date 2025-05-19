fb9fb2638a578ba13daccc57b2f45716216ffc9b
srijan_123 <srijan.giri@vyrazu.com>
2025-05-19 12:29:11 +0530

    NavLink vs Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,11 @@ function Navbar() {
                         <li><NavLink to={'/'} className='link'>Home</NavLink></li>
                         <li><NavLink to={'/about'} className='link'>About</NavLink></li>
                         <li><NavLink to={'/contact'} className='link'>Contact</NavLink></li>
-                        <li><NavLink to={'/login'} className='link'>Login</NavLink></li>
+                        <li><NavLink to={'/login'} className={({ isActive }) => isActive ? 'custom-active link' : 'link'}>Login</NavLink></li>
                         <li><NavLink to={'/college'} className="link">College</NavLink></li>
                         <li><NavLink to={'/users'} className='link'>Users</NavLink></li>
+
+                        {/* custom is active implementation */}
                         <li><NavLink to={'/users/list'} className='link'>List</NavLink></li>
                     </ul>
                 </div>
